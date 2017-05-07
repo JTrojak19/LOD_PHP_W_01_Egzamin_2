a7ca50b4a5c9aa2efe9e21c0570b831384ea2888
Joanna Trojak <joannatrojak19@gmail.com>
2017-05-07 11:19:38 +0200

    zad1

--- zad1.php
+++ zad1.php
@@ -1,5 +1,5 @@
 <?php
-$exercise_1a = "";
+$exercise_1a = "CREATE TABLE Destinations(id int not null auto_increment, user_id int not null, address varchar(255), lat decimal(13,10), long decimal(13,10), primary key(user_id), foreign key(id) references Users(id) ";
 
 $exercise_1b = "";
 
