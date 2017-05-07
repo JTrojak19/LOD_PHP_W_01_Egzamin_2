dced9dc2f4e8271df43ecccdbad33a78c25d4c33
Joanna Trojak <joannatrojak19@gmail.com>
2017-05-07 10:39:32 +0200

    zad5

--- zad5.php
+++ zad5.php
@@ -1,3 +1,46 @@
 <?php
+include "User.php"; 
+class VIPUser extends User
+{
+    public $vipCardNumber; 
+    
+    public function __construct($mail, $surname, $name, $vipCardNumber=null)
+    {
+        parent::__construct($mail, $surname, $name); 
+        $this->vipCardNumber=$vipCardNumber; 
+    }
+    
+    static public function checkCard($newNumber)
+    {
+        if ($newNumber > 999 && $newNumber%2 == 0)
+        {
+           return true;  
+        }
+        else 
+        {
+            return false; 
+        }
+    }
+    
+    public function setVipCardNumber($newCardNumber)
+    {
+        if ($this->checkCard($newCardNumber) == TRUE)
+        {
+            $this->vipCardNumber=$vipCardNumber; 
+        }
+        else 
+        {
+            $this->vipCardNumber=$vipCardNumber=null; 
+        }
+    }
+    public function getVipCardNumber()
+    {
+        return $this->vipCardNumber; 
+    }
+}
+$vip = new VIPUser('mail', 'nazwisko', 'imie', 123);
+
+$vip->getVipCardNumber(); 
+var_dump($vip); 
 
 
